326f77ad03f3e1d6f4652597a1792df67451a958
Thanakrit <thanakrit.than.biz@gmail.com>
2025-04-18 17:54:21 +0700

    assignment complete

--- server/middlewares/protect.js
+++ server/middlewares/protect.js
@@ -1,3 +1,26 @@
 // 🐨 Todo: Exercise #5
 // สร้าง Middleware ขึ้นมา 1 อันชื่อ Function ว่า `protect`
 // เพื่อเอาไว้ตรวจสอบว่า Client แนบ Token มาใน Header ของ Request หรือไม่
+import jwt from "jsonwebtoken";
+
+const protect = (req, res, next) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      message: "Forbidden",
+    });
+  }
+};
+
+export default protect;
